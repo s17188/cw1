131f9e5394ba917ba671ef50266a2b812b9baeb9
s17188 <s17188@pjatk.edu.pl>
2020-03-01 18:02:16 +0100

    completed exercise

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World! Git");
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl/");
+
+            if (result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[a-z]+",RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
         }
     }
 }
